3478696254895dcc5292410f47ebc6159a40a061
League Student <league@iMac-11.attlocal.net>
2019-07-24 18:09:39 -0700

    FinishedModule3:smile:
    Checkpoint

---
+++ src/Checkpoint32.java
@@ -0,0 +1,15 @@
+
+public class Checkpoint32 {
+	public static void main(String[] args) {
+		for(int i = 1; i < 8; i++){
+			if(i == 4) {
+				System.out.println(i);
+			}else {
+			   System.out.println(+i+ "potato");
+			}
+		}
+			System.out.println("more");
+		
+	}
+	
+}
